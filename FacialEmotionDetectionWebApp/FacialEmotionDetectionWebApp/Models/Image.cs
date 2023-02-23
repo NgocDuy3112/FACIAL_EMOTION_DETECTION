0b10230f59636f00ed7b8b7f0d39e33d02d1af1d
Nguyễn Lê Ngọc Duy <75831935+NgocDuy3112@users.noreply.github.com>
2023-02-23 19:23:40 +0700

    Deploy dev merging (#6)
    * Create ASP.NET project for the web application
    
    * Add files
    
    ---------
    
    Co-authored-by: DuyCoder100 <75831935+DuyCoder100@users.noreply.github.com>

---
+++ FacialEmotionDetectionWebApp/FacialEmotionDetectionWebApp/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FacialEmotionDetectionWebApp.Models
+{
+    public class Image
+    {
+        public string path { get; set; }
+        public string emotion { get; set; }
+
+        [NotMapped]
+        public IFormFile file { get; set; }
+    }
+}
